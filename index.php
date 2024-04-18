d63814b8ed23368d912b7e6be7850cd76f8fc28d
MarioBroi <inoix16@gmail.com>
2024-04-18 13:34:29 +0100

    add movies with objects in class Production

--- index.php
+++ index.php
@@ -1,10 +1,25 @@
 <?php
 class Production
 {
+    public $title;
+    public $lenguage;
+    public $vote;
 }
 
-$title = new Production();
-$lenguage = new Production();
-$vote = new Production();
+$dune = new Production();
+$fury = new Production();
+$avatar = new Production();
 
-var_dump($title, $lenguage, $vote);
+$dune->title = 'Dune';
+$dune->lenguage = 'EN';
+$dune->vote = 7;
+
+$fury->title = 'Fury';
+$fury->lenguage = 'EN';
+$fury->vote = 7;
+
+$avatar->title = 'Avatar';
+$avatar->lenguage = 'EN';
+$avatar->vote = 7;
+
+var_dump($dune, $fury, $avatar);
