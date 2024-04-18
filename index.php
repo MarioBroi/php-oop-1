2b1c5e1e6a871246c7ed0c48b61fbc57c68745af
MarioBroi <inoix16@gmail.com>
2024-04-18 14:06:12 +0100

    add constructor method

--- index.php
+++ index.php
@@ -1,25 +1,38 @@
 <?php
+/**
+ * Proction class
+ */
 class Production
 {
     public $title;
     public $lenguage;
     public $vote;
+
+    //constructor method
+    public function __construct(string $title, string $lenguage, int $vote)
+    {
+        $this->title = $title;
+        $this->lenguage = $lenguage;
+        $this->vote = $vote;
+    }
 }
 
-$dune = new Production();
-$fury = new Production();
-$avatar = new Production();
+$dune = new Production('Dune', 'EN', 7);
 
-$dune->title = 'Dune';
-$dune->lenguage = 'EN';
-$dune->vote = 7;
+//$dune->title = 'Dune';
+//$dune->lenguage = 'EN';
+//$dune->vote = 7;
 
-$fury->title = 'Fury';
-$fury->lenguage = 'EN';
-$fury->vote = 7;
+$fury = new Production('Fury', 'EN', 9);
 
-$avatar->title = 'Avatar';
-$avatar->lenguage = 'EN';
-$avatar->vote = 7;
+//$fury->title = 'Fury';
+//$fury->lenguage = 'EN';
+//$fury->vote = 9;
+
+$avatar = new Production('Avatar', 'EN', 6);
+
+//$avatar->title = 'Avatar';
+//$avatar->lenguage = 'EN';
+//$avatar->vote = 6;
 
 var_dump($dune, $fury, $avatar);
